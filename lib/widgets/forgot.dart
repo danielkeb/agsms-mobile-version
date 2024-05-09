75cd35327648ebcfe0f585d50a3b6743a0aceac5
Daniel <daniel@kali.kali>
2024-05-09 07:52:53 -0400

    corrected

--- lib/widgets/forgot.dart
+++ lib/widgets/forgot.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
+import 'package:abgsms/widgets/shorcode.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:abgsms/widgets/resetpassword.dart';
+import 'package:http/http.dart' as http;// Assuming the correct import for the Shortcode widget
 
 class ForgotPassword extends StatefulWidget {
   const ForgotPassword({Key? key}) : super(key: key);
@@ -12,10 +12,38 @@ class ForgotPassword extends StatefulWidget {
 
 class _ForgotPasswordState extends State<ForgotPassword> {
   final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _codeController = TextEditingController();
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  late int _userId;
-  ForgotFormState _formState = ForgotFormState.email;
+
+  Future<void> sendForgotPasswordRequest(String email) async {
+    try {
+      final response = await http.post(
+        Uri.parse('http://localhost:3333/auth/forget/shortcode'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(<String, String>{
+          'email': email,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
+        final userId = responseData['userId'];
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Shortcode(userId: userId),
+          ),
+        );
+      } else {
+        final responseData = json.decode(response.body);
+        //final userId = responseData['userId'];
+        _showErrorSnackBar('Failed to send reset code'+ responseData);
+      }
+    } catch (e) {
+      print('Error: $e');
+      _showErrorSnackBar('Failed to send reset code');
+    }
+  }
 
   void _showErrorSnackBar(String message) {
     ScaffoldMessenger.of(context).showSnackBar(
@@ -34,7 +62,6 @@ class _ForgotPasswordState extends State<ForgotPassword> {
       ),
       body: Center(
         child: Form(
-          key: _formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -56,134 +83,20 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                   },
                 ),
               ),
-              SizedBox(height: 20),
-              if (_formState == ForgotFormState.code)
-                Container(
-                  width: 300,
-                  child: TextFormField(
-                    controller: _codeController,
-                    decoration: InputDecoration(
-                      hintText: 'Enter shortcode',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter the shortcode';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-              SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () async {
-                  if (_formState == ForgotFormState.email) {
-                    final email = _emailController.text.trim();
-                    final forgotPasswordService = ForgotPasswordService();
-                    final response = await forgotPasswordService.sendForgotPasswordRequest(email);
-                    if (response != null) {
-                      _userId = response.userId;
-                      setState(() {
-                        _formState = ForgotFormState.code;
-                      });
-                    }
-                  } else if (_formState == ForgotFormState.code) {
-                    final code = _codeController.text.trim();
-                    final shortcodeVerificationService = ShortcodeVerificationService(_userId, code);
-                    final response = await shortcodeVerificationService.verifyShortcode();
-                    if (response != null && response.statusCode == 200) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ResetPassword(
-                            id: _userId.toString(),
-                          ),
-                        ),
-                      );
-                    } else {
-                      _showErrorSnackBar('Verification failed');
-                    }
+                onPressed: () {
+                  if (_emailController.text.isNotEmpty) {
+                    sendForgotPasswordRequest(_emailController.text);
+                  } else {
+                    _showErrorSnackBar('Please enter an email or phone number');
                   }
                 },
-                child: Text(_formState == ForgotFormState.email ? 'Send shortcode' : 'Verify Shortcode'),
+                child: Text('Send Reset Code'),
               ),
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-enum ForgotFormState { email, code }
-
-class ForgotPasswordService {
-  Future<ForgotPasswordResponse?> sendForgotPasswordRequest(String email) async {
-    try {
-      final response = await http.post(
-        Uri.parse('http://localhost:3333/auth/forget/shortcode'),
-        headers: <String, String>{
-          'Content-Type': 'application/json',
-        },
-        body: jsonEncode(<String, String>{
-          'email': email,
-        }),
-      );
-
-      if (response.statusCode == 200) {
-        final responseData = json.decode(response.body);
-        return ForgotPasswordResponse.fromJson(responseData);
-      } else {
-        return null;
-      }
-    } catch (e) {
-      print('Error: $e');
-      return null;
-    }
-  }
-}
-
-class ShortcodeVerificationService {
-  final int userId;
-  final String code;
-
-  ShortcodeVerificationService(this.userId, this.code);
-
-  Future<http.Response?> verifyShortcode() async {
-    try {
-      final response = await http.post(
-        Uri.parse('http://localhost:3333/verify/shortcode?userId=$userId'),
-        headers: <String, String>{
-          'Content-Type': 'application/json',
-        },
-        body: jsonEncode(<String, String>{
-          'shortcode': code,
-        }),
-      );
-
-      return response;
-    } catch (e) {
-      print('Error: $e');
-      return null;
-    }
-  }
-}
-
-class ForgotPasswordResponse {
-  final int userId;
-  final String message;
-  final int statusCode;
-
-  ForgotPasswordResponse({
-    required this.userId,
-    required this.message,
-    required this.statusCode,
-  });
-
-  factory ForgotPasswordResponse.fromJson(Map<String, dynamic> json) {
-    return ForgotPasswordResponse(
-      userId: json['userId'],
-      message: json['message'],
-      statusCode: json['statuscode'],
     );
   }
 }
--- lib/widgets/shorcode.dart
+++ lib/widgets/shorcode.dart
@@ -1,15 +1,129 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:abgsms/widgets/resetpassword.dart';
 
 class Shortcode extends StatefulWidget {
-  const Shortcode({super.key});
+  final int userId;
+
+  Shortcode({required this.userId});
 
   @override
   State<Shortcode> createState() => _ShortcodeState();
 }
 
 class _ShortcodeState extends State<Shortcode> {
+  http.Response? _verificationResponse;
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _codeController = TextEditingController();
+
+  Future<void> _verifyShortcode() async {
+    if (!_formKey.currentState!.validate()) return;
+    final code = _codeController.text.trim();
+    try {
+      final response = await _makeVerifyShortcodeRequest(code, widget.userId);
+      _handleSuccessResponse(response);
+    } catch (e) {
+      _handleError(e);
+    }
+  }
+
+  Future<http.Response> _makeVerifyShortcodeRequest(String code, int userId) async {
+    final uri = Uri.parse('http://localhost:3333/verify/shortcode?userId=$userId');
+    final headers = <String, String>{
+      'Content-Type': 'application/json',
+    };
+    final body = jsonEncode(<String, String>{
+      'shortcode': code,
+    });
+
+    return await http.post(uri, headers: headers, body: body);
+  }
+
+  void _handleSuccessResponse(http.Response response) {
+    setState(() {
+      _verificationResponse = response;
+    });
+  }
+
+  void _handleError(Object error) {
+    _showErrorSnackBar('Error: ${error.toString()}');
+    setState(() {
+      _verificationResponse = null;
+    });
+  }
+
+  void _showErrorSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: const Duration(seconds: 3),
+      ),
+    );
+  }
+
+  Widget _buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            controller: _codeController,
+            decoration: InputDecoration(
+              hintText: 'Enter shortcode',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter the shortcode';
+              }
+              return null;
+            },
+          ),
+          ElevatedButton(
+            onPressed: _verifyShortcode,
+            child: Text('Verify'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildVerificationResult() {
+    if (_verificationResponse!= null && _verificationResponse!.statusCode == 200) {
+      return TextButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ResetPassword(id: widget.userId.toString()),
+            ),
+          );
+        },
+        child: Text('Verify'),
+      );
+    } else if (_verificationResponse!= null) {
+      return Text('Verification failed');
+    } else {
+      return Container();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Short code verification'),
+      ),
+      body: Container(
+        width: 300,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildForm(),
+            _buildVerificationResult(),
+          ],
+        ),
+      ),
+    );
   }
 }
